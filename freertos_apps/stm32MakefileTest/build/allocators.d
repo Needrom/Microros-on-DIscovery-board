eed07a05b8da21026facaf0c954cbaf42eb876c4
Needrom <1264507247@qq.com>
2021-03-21 22:48:37 -0700

    delete build dir

--- freertos_apps/stm32MakefileTest/build/allocators.d
+++
@@ -1,4 +0,0 @@
-/home/mego/microros_ws/firmware/freertos_apps/stm32MakefileTest/build/allocators.o: \
- Core/Src/allocators.c Core/Inc/allocators.h
-
-Core/Inc/allocators.h:
--- freertos_apps/stm32MakefileTest/build/startup_stm32f407xx.d
+++
@@ -1,2 +0,0 @@
-/home/mego/microros_ws/firmware/freertos_apps/stm32MakefileTest/build/startup_stm32f407xx.o: \
- startup_stm32f407xx.s
